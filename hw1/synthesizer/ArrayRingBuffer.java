ad23f82c50fcf346d2abe035b6f267c2102bfdbf
ShowDownKiss <zyyskma@163.com>
2021-11-27 16:42:38 +0800

    first commit

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,12 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package synthesizer;
+import synthesizer.AbstractBoundedQueue;
+
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +23,11 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        this.rb = (T[]) new Object[capacity];
+        this.first = 0;
+        this.last = 0;
+        this.capacity = capacity;
+        this.fillCount = 0;
     }
 
     /**
@@ -28,8 +35,16 @@ public class ArrayRingBuffer<T>  {
      * throw new RuntimeException("Ring buffer overflow"). Exceptions
      * covered Monday.
      */
+
+    @Override
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[this.last] = x;
+        this.last = (this.last + 1) % this.capacity;
+        this.fillCount++;
     }
 
     /**
@@ -37,16 +52,59 @@ public class ArrayRingBuffer<T>  {
      * throw new RuntimeException("Ring buffer underflow"). Exceptions
      * covered Monday.
      */
+    @Override
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T res = rb[first];
+        this.first = (this.first - 1 + this.capacity) % this.capacity;
+        this.fillCount--;
+        return res;
     }
 
     /**
      * Return oldest item, but don't remove it.
      */
+    @Override
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
+    @Override
+    public Iterator<T> iterator() {
+        return new BufferIterator();
+    }
+
+    private class BufferIterator implements Iterator<T> {
+        private int pos;
+        private int num;
+
+        BufferIterator() {
+            pos = first;
+            num = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return num < fillCount;
+        }
+
+        @Override
+        public T next() {
+            T returnItem = rb[pos];
+            pos++;
+            if (pos == capacity) {
+                pos = 0;
+            }
+            num++;
+            return returnItem;
+        }
+    }
 }
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,103 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 30;
+    private static final int HEIGHT = 30;
+
+    public static class Positon {
+        private int x;
+        private int y;
+
+        public Positon(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private static int addHexagonHelper_1(int s, int y) {
+        int number;
+        if (y < s) {
+            number = s - 1 - y;
+        } else {
+            number = y - s;
+        }
+        return number;
+    }
+
+    private static int addHexagonHelper_2(int s, int y) {
+        int number;
+        if (y < s) {
+            number = y * 2 + s;
+        } else {
+            number = s + (s - 1) * 2 - (y - 2) * 2;
+        }
+        return number;
+    }
+
+    public static void addHexagon(TETile[][] world, Positon p, int s, TETile t) {
+        int rowHexagon;
+        int rowTri;
+        for (int y = 0; y < 2 * s; y++) {
+            rowTri = addHexagonHelper_1(s, y);
+            rowHexagon = addHexagonHelper_2(s, y);
+            for (int x = rowTri; x < rowTri + rowHexagon; x++) {
+                world[x+p.x][y+p.y] = t;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        int s = 3;
+        Positon[][] p = new Positon[5][9];
+        for (int y = 0; y < 9; y++) {
+            if (y == 0 || y == 8) {
+                int x = 2;
+                p[x][y] = new Positon((2*s-1)*x, y*s);
+            } else if (y % 2 == 0) {
+                for (int x = 0; x < 5; x = x + 2) {
+                    p[x][y] = new Positon((2*s-1)*x, y*s);
+                }
+            } else {
+                for (int x = 1; x < 4; x += 2) {
+                    p[x][y] = new Positon((2*s-1)*x, y*s);
+                }
+            }
+        }
+        TETile t = Tileset.GRASS;
+        addHexagon(world,p[0][2],s,t);
+        addHexagon(world,p[0][4],s,t);
+        addHexagon(world,p[1][7],s,t);
+        t = Tileset.FLOWER;
+        addHexagon(world,p[3][7],s,t);
+        addHexagon(world,p[4][6],s,t);
+        addHexagon(world,p[1][1],s,t);
+        t = Tileset.TREE;
+        addHexagon(world,p[2][8],s,t);
+        addHexagon(world,p[3][3],s,t);
+        addHexagon(world,p[4][4],s,t);
+        t = Tileset.MOUNTAIN;
+        addHexagon(world,p[0][6],s,t);
+        addHexagon(world,p[1][3],s,t);
+        addHexagon(world,p[1][5],s,t);
+        addHexagon(world,p[2][0],s,t);
+        addHexagon(world,p[2][2],s,t);
+        addHexagon(world,p[2][4],s,t);
+        addHexagon(world,p[2][6],s,t);
+        addHexagon(world,p[3][1],s,t);
+        t = Tileset.SAND;
+        addHexagon(world,p[3][5],s,t);
+        addHexagon(world,p[4][2],s,t);
+
+        ter.renderFrame(world);
+    }
 }
